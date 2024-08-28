51a79b3e414c3525a48b891a44162c4daeabd240
oscarlara <oscarlara80@gmail.com>
2024-08-28 13:37:27 +0200

    fix-mainContent

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,6 +1,5 @@
 // src/components/MainContent.jsx
 import React from "react";
-import Divider from "./Divider";
 
 const MainContent = ({ darkMode }) => {
   return (
@@ -287,7 +286,40 @@ const MainContent = ({ darkMode }) => {
         </div>
       </section>
 
-      <Divider color="blue-500" thickness="2px" marginY="8" />
+      {/* Mentoring Section */}
+      <section id="contact-mentoring-education" className="mt-12 w-full">
+        <h2 className="text-3xl font-semibold text-center text-blue-700 dark:text-blue-400 mb-6">
+          Mentoría y educación
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          {/* Mentoring Section */}
+          <div className="bg-white dark:bg-gray-900 shadow-md rounded-lg p-6 transform transition duration-500 hover:scale-105 hover:shadow-xl">
+            <h3 className="text-2xl font-semibold mb-2 text-gray-800 dark:text-gray-100">
+              Mentoria
+            </h3>
+            <p className="text-lg text-gray-900 dark:text-gray-300">
+              Como mentor especializado en nuevas tecnologías, guío a
+              estudiantes y profesionales en el dominio de las últimas
+              tendencias y herramientas tecnológicas. Con experiencia en
+              desarrollo web y pasión por la educación, ofrezco una perspectiva
+              única para ayudar a mis mentores a alcanzar sus objetivos.
+            </p>
+          </div>
+
+          {/* Education Section */}
+          <div className="bg-white dark:bg-gray-900 shadow-md rounded-lg p-6 transform transition duration-500 hover:scale-105 hover:shadow-xl">
+            <h3 className="text-2xl font-semibold mb-2 text-gray-800 dark:text-gray-100">
+              Actualización
+            </h3>
+            <p className="text-lg text-gray-900 dark:text-gray-300">
+              Me esfuerzo por mantenerme actualizado en las últimas tendencias y
+              herramientas tecnológicas, desde desarrollo web con React y
+              Tailwind CSS hasta inteligencia artificial y aprendizaje
+              automático.
+            </p>
+          </div>
+        </div>
+      </section>
 
       {/* Contact Section */}
       <section id="contact" className="mt-12 text-center w-full">
@@ -305,18 +337,6 @@ const MainContent = ({ darkMode }) => {
         >
           Envíame un correo
         </a>
-      </section>
-
-      {/* Mentoring Section */}
-      <section id="mentoring" className="mt-12 text-center w-full">
-        <h2 className="text-3xl font-semibold text-blue-700 dark:text-blue-400 mb-6">
-          Mentoría y Educación
-        </h2>
-        <p className="text-lg max-w-3xl mx-auto text-gray-900 dark:text-gray-300">
-          Además de mi experiencia como desarrollador, tengo una sólida
-          formación en mentoría y educación en informática y nuevas
-          tecnologías...
-        </p>
       </section>
     </div>
   );

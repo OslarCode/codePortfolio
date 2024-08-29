451c49a73f54622718b6d33550266cb80f9c93b1
oscarlara <oscarlara80@gmail.com>
2024-08-29 21:01:59 +0200

    fix-delete-Loading-Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // src/App.jsx
 import { useState, useEffect } from "react";
-import LoadingScreen from "./components/LoadingScreen";
 import MainContent from "./components/MainContent";
 import Navbar from "./components/Navbar";
 import Contacto from "./components/Contacto";
@@ -9,10 +8,6 @@ import PropTypes from "prop-types";
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [darkMode, setDarkMode] = useState(false);
-
-  const handleLoadingComplete = () => {
-    setIsLoading(false);
-  };
 
   useEffect(() => {
     if (darkMode) {
@@ -26,12 +21,7 @@ function App() {
     <div className={`${darkMode ? "dark" : ""}`}>
       <div className="min-h-screen bg-white dark:bg-gray-900">
         <Navbar darkMode={darkMode} setDarkMode={setDarkMode} outlined={true} />
-        {isLoading ? (
-          <LoadingScreen onLoaded={handleLoadingComplete} />
-        ) : (
-          <MainContent darkMode={darkMode} />
-        )}
-
+        isLoading ? () : <MainContent darkMode={darkMode} />
         <Contacto darkMode={darkMode} />
       </div>
     </div>
